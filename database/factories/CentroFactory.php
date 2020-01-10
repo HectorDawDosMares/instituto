6e8dbb763c341e2b8474f1d0bc156520d4815886
Tharduk <tharduk@gmail.com>
2020-01-10 11:52:50 +0100

    Incorporando el modelo Centro y relacionándolo con el de User a través de coordinador

--- database/factories/CentroFactory.php
+++ database/factories/CentroFactory.php
@@ -5,22 +5,12 @@
 use App\Centro;
 use Faker\Generator as Faker;
 
-/*
-|--------------------------------------------------------------------------
-| Model Factories
-|--------------------------------------------------------------------------
-|
-| This directory should contain each of the model factory definitions for
-| your application. Factories provide a convenient way to generate new
-| model instances for testing / seeding your application's database.
-|
-*/
-
 $factory->define(Centro::class, function (Faker $faker) {
-    return [
-        'codigo' => $faker->unique()->randomNumber;
-        'nombre' => $faker->company;
-        'web' => $faker->url;
-        'verificado' => $faker->boolean($chanceOfGettingTrue = 80);
+   return [
+        'codigo' => $faker->unique()->randomNumber,
+        'nombre' => $faker->company,
+        'web' => $faker->url,
+        //'coordinador' => factory(App\User::class),
+        'verificado' => $faker->boolean($chanceOfGettingTrue = 80)
     ];
 });
